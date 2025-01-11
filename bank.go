7d7152d7667261b9919d9616ffaf3773b32b585b
Aries Visibee <aries@visibleone.com>
2025-01-12 01:01:41 +0700

    switch statement

--- bank.go
+++ bank.go
@@ -7,6 +7,7 @@ func main() {
 	fmt.Println("Welcome to Go Bank!")
 
 	for i := 0; i < 2; i++ {
+
 		fmt.Println("What do you wnat to do?")
 		fmt.Println("1. Check balance")
 		fmt.Println("2. Deposit money")
@@ -16,7 +17,37 @@ func main() {
 		var choice int
 		fmt.Print("Your choice: ")
 		fmt.Scan(&choice)
-
+		switch choice {
+		case 1:
+			fmt.Println("Your balance is", accountBalance)
+		case 2:
+			fmt.Print("Your Deposit: ")
+			var depositAmount float64
+			fmt.Scan(&depositAmount)
+			if depositAmount <= 0 {
+				fmt.Println("Invalid amount. Must be greater than 0.")
+			}
+			accountBalance += depositAmount
+			fmt.Println("Balance Updated! New amount: ", accountBalance)
+		case 3:
+			fmt.Print("Withdraw amount: ")
+			var withdrawAmount float64
+			fmt.Scan(&withdrawAmount)
+			if withdrawAmount <= 0 {
+				fmt.Println("Invalid amount. Must be greater than 0.")
+				continue
+			}
+			if withdrawAmount > accountBalance {
+				fmt.Println("Invalid amount. You can't withdraw more than you have.")
+				continue
+			}
+			accountBalance -= withdrawAmount
+			fmt.Println("Balance Updated! New amount: ", accountBalance)
+		default:
+			fmt.Println("Goobye!")
+			fmt.Println("Thanks for choosing our bank")
+			return
+		}
 		if choice == 1 {
 			fmt.Println("Your balance is", accountBalance)
 		} else if choice == 2 {
@@ -34,21 +65,21 @@ func main() {
 			fmt.Scan(&withdrawAmount)
 			if withdrawAmount <= 0 {
 				fmt.Println("Invalid amount. Must be greater than 0.")
-				return
+				continue
 			}
 			if withdrawAmount > accountBalance {
 				fmt.Println("Invalid amount. You can't withdraw more than you have.")
-				return
+				continue
 			}
 			accountBalance -= withdrawAmount
 			fmt.Println("Balance Updated! New amount: ", accountBalance)
 		} else {
 			fmt.Println("Goobye!")
+			fmt.Println("Thanks for choosing our bank")
 			// return
-			break
+			// break
 		}
 
-		fmt.Println("Thanks for choosing our bank")
 	}
 
 }

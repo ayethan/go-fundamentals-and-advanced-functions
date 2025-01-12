ef687d91d5dd1d482c0059a56c579d0ab2e95601
Aries Visibee <aries@visibleone.com>
2025-01-12 21:37:46 +0700

    Task1 error halding

--- profit_calculator.go
+++ profit_calculator.go
@@ -1,15 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 func main() {
 	// var revenue float64
 	// var expenses float64
 	// var taxRate float64
 
-	revenue := getUserInput("Revenue: ")
-	expenses := getUserInput("Expenses: ")
-	taxRate := getUserInput("TaxRate: ")
+	revenue, err1 := getUserInput("Revenue: ")
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	return
+	// }
+
+	expenses, err2 := getUserInput("Expenses: ")
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	return
+	// }
+
+	taxRate, err3 := getUserInput("TaxRate: ")
+
+	if err1 != nil || err2 != nil || err3 != nil {
+		fmt.Println(err1)
+		return
+	}
 
 	ebt, profit, ratio := calculatFinancials(revenue, expenses, taxRate)
 
@@ -25,9 +43,12 @@ func calculatFinancials(revenue, expenses, taxRate float64) (float64, float64, f
 	return ebt, profit, ratio
 }
 
-func getUserInput(infoText string) float64 {
+func getUserInput(infoText string) (float64, error) {
 	var userInput float64
 	fmt.Print(infoText)
 	fmt.Scan(&userInput)
-	return userInput
+	if userInput <= 0 {
+		return 0, errors.New("value must be a positive number")
+	}
+	return userInput, nil
 }
